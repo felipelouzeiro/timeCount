b5bee04ffde22a7f1ffc3c55cb7c8fdc7ca3c829
felipe <fl.louzeiro@gmail.com>
2022-05-19 01:19:48 -0300

    feat: implementa salvamento do timer em localstorage

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-function start() {
-  setInterval(countdownTimer, 1000)
+let finalDate;
+let intervalId;
+
+const second = 1000;
+const minute = 60 * second;
+const hour = 60 * minute;
+const day = 24 * hour;
+
+// verifica ao carregar a página
+if (localStorage.getItem("timer")) {
+  finalDate = localStorage.getItem("timer");
+  intervalId = setInterval(countdownTimer, 1000);
 }
 
-function reset() {
-  document.querySelector("h1").innerHTML = "⏱️";
+function start() {
+  const selectDate = document.querySelector("input").value;
+  finalDate = new Date(selectDate).getTime();
+
+  localStorage.setItem("timer", finalDate);
+  
+  intervalId = setInterval(countdownTimer, 1000)
 }
 
 function countdownTimer() {
-  const selectDate = document.querySelector("input").value;
-  let finalDate = new Date(selectDate).getTime();
   let nowDate = new Date().getTime();
   let diff = (finalDate - nowDate);
-
-  const second = 1000;
-  const minute = 60 * second;
-  const hour = 60 * minute;
-  const day = 24 + hour;
 
   let countDay = Math.floor(diff / day);
   let countHour = Math.floor((diff % day) / hour) + 3;
   let countMinute = Math.floor((diff % hour) / minute);
   let countSecond = Math.floor((diff % minute) / second);
 
-  console.log(nowDate, countDay, countHour, countMinute, countSecond);
-
   document.querySelector("h1").innerHTML = `${countDay}d ${countHour}h ${countMinute}m ${countSecond}s`
 }
+
+  function reset() {
+    document.querySelector("h1").innerHTML = "⏱️";
+    clearInterval(intervalId);
+
+    localStorage.removeItem("timer")
+  }
