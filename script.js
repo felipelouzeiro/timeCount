9a362b5664e3aa9f730cec03be4828c6ec9d95a0
felipe <fl.louzeiro@gmail.com>
2022-05-19 00:10:06 -0300

    feat: implementa botao reset e o setInterval

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 function start() {
+  setInterval(countdownTimer, 1000)
+}
+
+function reset() {
+  document.querySelector("h1").innerHTML = "⏱️";
+}
+
+function countdownTimer() {
   const selectDate = document.querySelector("input").value;
   let finalDate = new Date(selectDate).getTime();
   let nowDate = new Date().getTime();
   let diff = (finalDate - nowDate);
 
-  let second = 1000;
-  let minute = 60 * second;
-  let hour = 60 * minute;
-  let day = 24 + hour;
+  const second = 1000;
+  const minute = 60 * second;
+  const hour = 60 * minute;
+  const day = 24 + hour;
 
   let countDay = Math.floor(diff / day);
   let countHour = Math.floor((diff % day) / hour) + 3;
@@ -17,9 +25,4 @@ function start() {
   console.log(nowDate, countDay, countHour, countMinute, countSecond);
 
   document.querySelector("h1").innerHTML = `${countDay}d ${countHour}h ${countMinute}m ${countSecond}s`
-}
-
-function restart() {
-  let content = document.querySelector("input").value;
-  console.log(content);
 }
